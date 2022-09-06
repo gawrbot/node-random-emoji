301f3f79e2fcf919d6841a64606479ed52bc4554
Rhea Dieberger <rdieberger@gmail.com>
2022-09-06 12:12:44 +0200

    wrote random and input emoji program Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+//common js syntax, don't use it
+// const emoji = require('node-emoji');
+
+//right way --> using ES js syntax
+import emoji from 'node-emoji';
+import { argv } from 'process';
+
+if (argv[2] && emoji.find(argv[2])) {
+  console.log(emoji.get(argv[2]));
+} else if (argv[2]) {
+  console.log(emoji.get('cry') + " emoji '" + argv[2] + "' couldn't be found");
+} else {
+  console.log(emoji.random().emoji);
+}
